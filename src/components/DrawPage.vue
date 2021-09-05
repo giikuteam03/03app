14f04c22009bb303a164b12eeb3ee7f7719c3e4d
y-higashimura <89777488+y-higashimura@users.noreply.github.com>
2021-09-05 11:32:10 +0900

    名前の描画、検索先の円描画、各円のクリック判定取得

--- src/components/DrawPage.vue
+++ src/components/DrawPage.vue
@@ -1,8 +1,13 @@
 <template>
   <div class="DrawPage">
-      <p>{{message}}</p>
-      <p></p>
-      <canvas id="canvas" :height="canvas_height" :width="canvas_width" ></canvas>
+        <p>{{message}}</p>
+        <p></p>
+        <canvas
+            id="canvas"
+            :height="canvas_height"
+            :width="canvas_width"
+            v-on:click="canvesClickEvent">
+        </canvas>
   </div>
 </template>
 
@@ -13,6 +18,42 @@ class Vector2{
         this.y = y;
     }
 }
+
+class Info{
+    constructor(){
+        this.name;
+    }
+}
+
+class Circle{
+    constructor(x, y, r) {
+      this.postion = new Vector2(x,y);
+      this.r = r;
+      this.color = '#ff000000';
+      this.Info = new Info();
+    }
+
+    setColor(a, r, g, b){
+        this.color = "rgba(" + [r, g, b, a] + ")";
+        return this;
+    }
+
+    draw(ctx) {
+      ctx.save();
+      ctx.beginPath();
+      ctx.arc(this.postion.x, this.postion.y, this.r, 0, Math.PI * 2, true);
+      ctx.fillStyle = this.color;
+      ctx.fill();
+      ctx.restore();
+    }
+
+    isHit(x, y) {
+      return (
+        Math.pow(this.postion.x - x, 2) + Math.pow(this.postion.y - y, 2) <= Math.pow(this.r, 2)
+      );
+    }
+}
+
 
 export default {
     name: 'DrawPage',
@@ -26,52 +67,103 @@ export default {
         return{
             message : 'Draw!!',
             num : 1,
-            canvas_height:1024,
-            canvas_width:1024,
-            //search_results_name:["A","B","C","D","E"], // 検索元の単語
-            //search_results_volume:[20,20,20,20,20], // 検索結果の件数
-            circle:[],
+            canvas_width:990,
+            canvas_height:540,
+            select_circles:[],
+            search_circles:[],
+            center_cricle:null,
         }
     },
     mounted : function(){
+        this.canvas = document.getElementById('canvas');
+        let ctx = this.canvas.getContext('2d');
+
+        const center = new Vector2(this.canvas_width * 0.5, this.canvas_height * 0.5)
+        const center_r = center.y * 0.3;
+
+        this.center_cricle = new Circle(center.x, center.y, center_r * 1.8);
+        this.center_cricle.setColor(0.10, 0, 0, 0);
+
+        var sum_volume = this.select_volumes
+            .reduce(function(sum, element){
+                return sum + element;
+            }, 0);
+        var sum_rad = 0;
+        for(let i = 0; i < this.select_volumes.length; i++){
+            let rate = this.select_volumes[i] / sum_volume;
+            let rad = Math.PI * rate;
+            let circle = new Circle(
+                center.x + Math.sin(sum_rad + rad) * center_r,
+                center.y + Math.cos(sum_rad + rad) * -center_r,
+                center_r * rad);
+            circle.setColor(0.25, 0, 255, 0);
+            circle.Info.name = this.select_names[i];
+            this.select_circles.push(circle);
+            sum_rad += 2 * rad;
+        }
+
+        sum_volume = this.search_results_volumes
+            .reduce(function(sum, element){
+                return sum + element;
+            }, 0);
+        sum_rad = 0;
+        for(let i = 0; i < this.search_results_volumes.length; i++){
+            let rate = this.search_results_volumes[i] / sum_volume;
+            let rad = Math.PI * rate;
+            let circle = new Circle(
+                center.x + Math.sin(sum_rad + rad) * center_r * 2.75,
+                center.y + Math.cos(sum_rad + rad) * center_r * 2.75,
+                center_r * rad);
+            circle.setColor(0.25, 0, 0, 255);
+            circle.Info.name = this.search_results_names[i];
+            this.search_circles.push(circle);
+            sum_rad += 2 * rad;
+        }
         // 起動--wait-->描画とすると上手くいく
         setTimeout(() => {
-            var center = new Vector2(this.canvas_width * 0.5, this.canvas_height * 0.5)
-            var center_r = center.x * 0.25;
-            var sum_volume = this.select_volumes
-                .reduce(function(sum, element){
-                    return sum + element;
-                }, 0);
-
-            var canvas = document.getElementById('canvas');
-            var c = canvas.getContext('2d');
-
-            var sum_rad = 0;
-            for(let i = 0; i < this.select_volumes.length; i++){
-                var rate = this.select_volumes[i] / sum_volume;
-                var rad = Math.PI * rate;
-
-                c.fillStyle = "rgba(" + [0, 255, 0, 0.25] + ")";
-                // パスの開始
-                c.beginPath();
-                c.arc(
-                    center.x + Math.sin(sum_rad + rad) * center_r,
-                    center.y + Math.cos(sum_rad + rad) * center_r,
-                    center_r * rad, 0, 2 * Math.PI, false);
-                // 描画
-                c.fill();
-
-                sum_rad += 2 * rad;
-            }
+            // 描画
+            this.center_cricle.draw(ctx);
+            this.select_circles.forEach(element => {
+                element.draw(ctx);
+                ctx.fillText(element.Info.name, element.postion.x, element.postion.y);
+            });
+            this.search_circles.forEach(element => {
+                element.draw(ctx);
+                ctx.fillText(element.Info.name, element.postion.x, element.postion.y);
+            });
         }, 10);
     },
+    methods:{
+        canvesClickEvent:function(event){
+            // マウスの座標をCanvas内の座標とあわせるため
+            const rect = this.canvas.getBoundingClientRect();
+            const point = new Vector2(
+                event.clientX - rect.left,
+                event.clientY - rect.top
+            );
+            this.select_circles.forEach(element => {
+                if(element.isHit(point.x, point.y)){
+                    console.log("click!");
+                }
+            });
+            this.search_circles.forEach(element => {
+                if(element.isHit(point.x, point.y)){
+                    console.log("click!");
+                }
+            });
+        },
+        DrawInit:function(){
+
+        }
+    }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
- canvas {
+canvas {
         border: 1px solid silver;
-        width: 50%;
-    }
+        /* ↓ click 処理がバグる */
+        /* width: 100%; */
+}
 </style>
